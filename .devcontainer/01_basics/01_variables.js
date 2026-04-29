b359c26a963668717049f28e999f3be18da6280a
Pari Sah <pari0517sah@gmail.com>
2026-04-29 09:01:30 +0000

    dataTypes

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId=122234
+let accountEmail="pari0517sah@gmail.com"
+var accountPassword="pari0517"
+accountCity="jaipur"
+let accountState;//will give undefined value
+
+//accountId=2
+accountEmail="hcs@gmail.com"
+accountPassword="jhbfuh"
+accountCity="bihar"
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
+/* prefer not to use var because of issue in block 
+scope and functional scope */
---
+++ .devcontainer/01_basics/02_dataTypes.js
@@ -0,0 +1,9 @@
+"use strict" //treat all js code as newer version
+//alert(3+3) //we are using node.js,not brower 
+let name="pari" //string 
+let age=21
+let isLoggedIn=false
+console.log(typeof"pari");
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
+
